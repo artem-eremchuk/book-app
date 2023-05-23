e97c9d96941b81044cbce12428e647f42a429ca0
Artem Eremchuk <eremchuk.it@gmail.com>
2023-05-23 18:10:12 +0300

    Add on-change library

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import { MainView } from "./views/main/main.js";
 
 class App {
   routes = [{ path: "", view: MainView }];
+  appState = {
+    favorites: [],
+  };
+
   constructor() {
     window.addEventListener("hashchange", this.route.bind(this));
     this.route();
@@ -13,7 +17,7 @@ class App {
     }
 
     const view = this.routes.find((r) => r.path === location.hash).view;
-    this.currentView = new view();
+    this.currentView = new view(this.appState);
     this.currentView.render();
   }
 }
